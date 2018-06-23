7905f6378b87496149150b3ac8cf529dbe9f3d45
Jack353 <jvc353123@gmail.com>
2018-06-22 20:38:57 -0400

    Finishes numeral_systems

---
+++ one/numeral_systems/hex/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d - %x \n", 42, 42)
+	fmt.Printf("%d - %X \n", 42, 42)
+	fmt.Printf("%d - %#x \n", 42, 42)
+	fmt.Printf("%d - %#X \n", 42, 42)
+}
