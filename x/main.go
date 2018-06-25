718eef76d2f0d2d62c7c94781c4a7a067f4c688b
Jack353 <jvc353123@gmail.com>
2018-06-24 22:30:41 -0400

    fixes x

--- x/main.go
+++ x/main.go
@@ -1,1 +1,11 @@
-package x
+package main
+
+import "fmt"
+
+func main() {
+	a := "juice"
+	b := 509
+
+	fmt.Printf("%b \t %d \t %x \n", 42, 42, 42)
+	fmt.Printf("%s \n %d \t %b \t %x \t %X \n", a, b, b, b, b)
+}
